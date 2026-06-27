c746fa2b8e4719faf797b5c8eea04bee53c38f08
Vesk <bohnyak.k3@gmail.com>
2026-06-27 16:12:53 +0800

    nc

--- download/download.js
+++ download/download.js
@@ -1,3 +1,6 @@
+const DataFile = "27 July"
+const DataFileForm = new Date('2026-06-27')
+
 document.addEventListener("DOMContentLoaded", function() {
     const button = document.getElementById('downloadBtn');
     button.addEventListener('click', function() {
@@ -5,4 +8,20 @@ document.addEventListener("DOMContentLoaded", function() {
         link.href = "../bild/White World Launcher.exe";
         link.click();
     });
+
+    const Data = document.getElementById('dataP');
+    Data.textContent = Data.textContent + DataFile;
+    const DayAgo = document.getElementById('dayAgoP');
+
+    const pastDate = DataFileForm;
+    const now = new Date();
+    const daysPassed = getDaysDifference(pastDate, now);
+
+    DayAgo.textContent = daysPassed + DayAgo.textContent;
 });
+
+function getDaysDifference(date1, date2) {
+  const diffMs = Math.abs(date2 - date1);
+  const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+  return diffDays;
+}
